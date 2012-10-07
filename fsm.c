7996d981cc3c9364f89ae1fb85e6a9f6abbd1081
dimmddr <dmitrius.one@gmail.com>
2012-10-07 21:04:22 +0400

    Тестирую, и правлю баги. По состоянию на текущий момент - что-то странное с таблицей переходов, в остальном вроде все ок.

--- fsm.c
+++ fsm.c
@@ -22,11 +22,13 @@ typedef unsigned __int64 ticks;
 #define STARTLINE 16
 #define PREFIXCOUNT 11
 
-VOID _stdcall disasm(PVOID, PVOID, UINT);
+INT _stdcall disasm(PVOID, PVOID, UINT);
 
 //global variables
 PVOID g_va;
-UINT conditionTable[COND][BYTE];
+PVOID ct;
+PVOID pa;
+UINT8 conditionTable[COND*BYTE];
 UINT8 prefixArray[PREFIXCOUNT];
 char *qwer;
 
@@ -48,39 +50,6 @@ UINT8 getByte() {
 	return res;
 }
 
-void prefixArrayInit() {
-	prefixArray[0] = 0xf0;
-	prefixArray[1] = 0x65;
-	prefixArray[2] = 0x2f;
-	prefixArray[3] = 0x3f;
-	prefixArray[4] = 0x66;
-	prefixArray[5] = 0x67;
-	prefixArray[6] = 0x2e;
-	prefixArray[7] = 0x3e;
-	prefixArray[8] = 0x36;
-	prefixArray[9] = 0x26;
-	prefixArray[10] = 0x64;
-}
-
-PVOID getPrefix(INSTRUCTION *instr) {
-	_asm{
-		mov esi, g_va
-		cld
-	start:	
-		lea edi, prefixArray
-		mov ecx, 11 ;количество префиксов
-		;REPNE SCAS m16 Find AX, starting at ES:[(E)DI]
-		;Compare AL with byte at ES:(E)DI or RDI then set status flags
-		repe scasb
-		;repne scasb
-		;jnz q
-		jz start
-		;set bit
-		;jmp start
-	q:	mov g_va, esi
-	}
-}
-
 void initializeFSM(PVOID va) {
 	g_va = va;
 }
@@ -90,55 +59,17 @@ void initializeTable() {
 	int f;
 	FILE *in, *fopen();
 	in = fopen("result_table.txt", "r");
-	for(i = 0; i < COND; ++i)
-		for(ii = 0; ii < BYTE; ++ii){
-			fscanf(in, "%u", &conditionTable[i][ii]);
+	for(i = 0; i < COND*BYTE; ++i){
+			fscanf(in, "%u", &conditionTable[i]);
 			}
 	fclose(in);
 }
 
-void getInstruction(INSTRUCTION *instr) {
-	UINT state = 0;
-	int next = -1;
-	UINT8 b;
-	/*
-	printf("before %x\n",CURRENT_BYTE);
-	getch();
-	*/
-	getPrefix(instr);
-	/*
-	printf("after %x\n",CURRENT_BYTE);
-	getch();
-	*/
-	//printf("instr\n");
-	/*
-	while (next != 0) {
-		if(0 <= next) {
-			state = next;
-			}
-		b = getByte();
-		next = conditionTable[state][b];
-	}
-	*/
-	
-	for(;0 != next;) {
-		if(0 < next) 
-			state = (UINT) next;
-		b = getByte();
-		//printf("next 1 %d\n", next);
-		//printf("state in cycle %d\n", state);
-		next = conditionTable[state][b];
-		//printf("next 2 %d\n", next);
-	} 
-	//printf("state %u\n", state);
-	instr->state = state;
-}
+
 
 void main(int argc, PSTR argv[])
 {
 	UINT resInstr[TRYCOUNT];
-	UINT8 state = 0;
-	UINT8 b;
 	LOADED_IMAGE image;
 	PSTR imageFilename;
 	int i;
@@ -146,9 +77,6 @@ void main(int argc, PSTR argv[])
 	PVOID va;
 	unsigned __int64 tickCount;
 	INSTRUCTION instr[COUNT];
-	
-	for(i = 0; i < COUNT; ++i)
-		instr[i].state = 0;
 	
 	//imageFilename = argv[1];
 	imageFilename = "test_prefix.exe";
@@ -160,15 +88,16 @@ void main(int argc, PSTR argv[])
 	va = ImageRvaToVa(image.FileHeader, image.MappedAddress,
 						  image.FileHeader->OptionalHeader.BaseOfCode, NULL);
 	initializeTable();
-	prefixArrayInit();
 	_asm{
-			lfence
+		lea eax, conditionTable
+		mov ct, eax
+		lfence
 		}
-	tickCount = getticks();
-	
-		disasm(g_va, &conditionTable, COUNT);
-	tickCount = (getticks() - tickCount);	
-	printf("%d \n",tickCount);
+	tickCount = disasm(g_va, ct, COUNT);	
+	printf("in c: %d \n",tickCount);
+	//printf("%x \n",conditionTable);
+	//printf("%x \n",&conditionTable);
+	//printf("%x \n",ct);
 	/*
 	for(ii = 0; ii < TRYCOUNT; ++ii) {
 		initializeFSM(va);
@@ -194,5 +123,58 @@ void main(int argc, PSTR argv[])
 	for(ii = 0; ii < TRYCOUNT; ++ii) 
 		printf("%d \n",resInstr[TRYCOUNT]/COUNT);
 	
+	*/
+	/*
+	void getInstruction(INSTRUCTION *instr) {
+	UINT state = 0;
+	int next = -1;
+	UINT8 b;
+	
+	getPrefix(instr);
+	
+	for(;0 != next;) {
+		if(0 < next) 
+			state = (UINT) next;
+		b = getByte();
+		next = conditionTable[state][b];
+	} 
+	instr->state = state;
+}
+	*/
+	/*
+	
+void prefixArrayInit() {
+	prefixArray[0] = 0xf0;
+	prefixArray[1] = 0x65;
+	prefixArray[2] = 0x2f;
+	prefixArray[3] = 0x3f;
+	prefixArray[4] = 0x66;
+	prefixArray[5] = 0x67;
+	prefixArray[6] = 0x2e;
+	prefixArray[7] = 0x3e;
+	prefixArray[8] = 0x36;
+	prefixArray[9] = 0x26;
+	prefixArray[10] = 0x64;
+}
+	*/
+	/*
+	PVOID getPrefix(INSTRUCTION *instr) {
+	_asm{
+		mov esi, g_va
+		cld
+	start:	
+		lea edi, prefixArray
+		mov ecx, 11 ;количество префиксов
+		;REPNE SCAS m16 Find AX, starting at ES:[(E)DI]
+		;Compare AL with byte at ES:(E)DI or RDI then set status flags
+		repe scasb
+		;repne scasb
+		;jnz q
+		jz start
+		;set bit
+		;jmp start
+	q:	mov g_va, esi
+	}
+	}
 	*/
 }
--- fsmA.asm
+++ fsmA.asm
@@ -5,6 +5,7 @@
 	option	casemap:none
 	
 	BSIZE equ 15
+	PrC equ 11 ;количество префиксов
 	
 	include user32.inc
 	includelib user32.lib
@@ -14,60 +15,88 @@
 	.data
 	ifmt	db "%0lu", 0
 	outp	db BSIZE dup(?)
+	prefix 	dw PrC dup(?)
 	
-;disasm PROTO :dword, :dword, :dword
-	
-	.code
 public disasm 
+	.code
 	disasm proc byteAddress, tableAddress, count
 	;Пока что передаем в функцию три параметра: адрес первого байта, адрес таблицы состояний, количество итераций
-	;сохраняем значения системных регистров
 	push ebp
-	push esi
-	push edi
 	
 	mov ecx, count
-	mov edi, byteAddress
-	mov esi, tableAddress
+	mov esi, byteAddress
+	mov ebp, tableAddress
+	call prefixInit
+	lea edi, prefix
+	lfence
+	rdtsc
+	push eax
+	push edx
+	
 	push ecx ;сохраняем количество итераций, для следующего цикла
 	qwer:
-		call getInstruction
+		;call getInstruction
 		;вообще не лучший вариант - использовать стек. Может стоит потом массив сделать
 		;push eax
-	loop qwer
+	;loop qwer
 	pop ecx
-	
-	invoke GetStdHandle, -11
-	
-	;print:
+	;invoke GetStdHandle, -11
+	print:
+	add esi, esi
+	add esi, esi
+	add esi, esi
+	add esi, esi
+	add esi, esi
+	add esi, esi
+	add esi, esi
+	add esi, esi
+	add esi, esi
+	add esi, esi
+	add esi, esi
+	add esi, esi
+	add esi, esi
+	add esi, esi
+	add esi, esi
+	add esi, esi
+	add esi, esi
+	add esi, esi
+	add esi, esi
+	add esi, esi
 	;	pop ebx
 	;	invoke	wsprintf, addr outp, addr ifmt, ebx
 	;	invoke	WriteConsoleA, eax, addr outp, 10, 0, 0
 	;loop print
 	
-	
-	;восстанавливаем значения системных регистров
-	pop edi
-	pop esi
+	rdtsc
+	mov ebx, eax
+	mov ecx, edx
+	pop edx
+	pop eax
+	sub ebx, eax
+	sub ecx, edx
+	mov edx, ebx
+	invoke	wsprintf, addr outp, addr ifmt, ebx
+	invoke GetStdHandle, -11
+	invoke	WriteConsoleA, eax, addr outp, 10, 0, 0	
+	mov eax, ebx
 	pop ebp
-	
 	ret
 	disasm endp
-	
-;адрес следующего байта хранится в регистре edi
+	;адрес следующего байта хранится в регистре edi
 ;адрес таблицы переходов для опкодов - в esi
 getInstruction proc
-	
 	call getPrefix
 	
 	mov ebx, 0
 	instructionStart:
-		lodsb
 		mov edx, ebx ;сохраняем текущее состояние
-		shl ebx, 9 ;умножаем на ширину таблицы и еще на 2, потому что ячейки в таблице 2 байта
-		shl eax, 1 ; по аналогичной причине умножаем входной байт на 2
+		shl ebx, 8 ;умножаем на ширину таблицы
+		;shl ebx, 9 ;умножаем на ширину таблицы и еще на 2, потому что ячейки в таблице 2 байта
+		;shl eax, 1 ; по аналогичной причине умножаем входной байт на 2
 		add eax, ebx ;получаем смещение, по которому хранится следующее состояние
-		mov ebx, [esi + eax] ;получаем слдующее состояние
+		mov bl, [ebp + eax] ;получаем следующее состояние
+		mov eax, 0
+		lodsb
 		or ebx, ebx ;сравниваем его с 0
 		jnz instructionStart
 		mov eax, edx;возвращаем последнее не нулевое состояние
@@ -78,7 +107,11 @@ getInstruction endp
 
 getPrefix proc
 		cld
+		mov ebx, edi
 	prefixStart:	
+		mov edi, ebx
+		mov eax, 0
+		lodsb
 		mov ecx, 11 ;количество префиксов
 		;REPNE SCAS m16 Find AX, starting at ES:[(E)DI]
 		;Compare AL with byte at ES:(E)DI or RDI then set status flags
@@ -87,8 +120,43 @@ getPrefix proc
 		;jnz q
 		jz prefixStart
 		;set bit
-		;jmp start
+		;jmp prefixStart
 	q:	ret
 getPrefix endp
-	
-end disasm
+
+prefixInit proc
+	mov ecx, 0
+	mov [prefix + ecx],0f0h
+	inc ecx
+	mov [prefix + ecx],065h
+	inc ecx
+	mov [prefix + ecx],02fh
+	inc ecx
+	mov [prefix + ecx],03fh
+	inc ecx
+	mov [prefix + ecx],066h
+	inc ecx
+	mov [prefix + ecx],067h
+	inc ecx
+	mov [prefix + ecx],02eh
+	inc ecx
+	mov [prefix + ecx],03eh
+	inc ecx
+	mov [prefix + ecx],036h
+	inc ecx
+	mov [prefix + ecx],026h
+	inc ecx
+	mov [prefix + ecx],064h
+	invoke GetStdHandle, -11
+	mov ecx, 11
+	print:
+	;mov bx, [prefix + ecx]
+	;push ecx
+	;invoke	wsprintf, addr outp, addr ifmt, ebx
+	;invoke	WriteConsoleA, eax, addr outp, 10, 0, 0
+	;pop ecx
+	;loop print
+	xor eax, eax
+	ret
+prefixInit endp
+end
