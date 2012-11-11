adef5f376ebf4032b8dbf826b0837954cd98260d
dimmddr <dmitrius.one@gmail.com>
2012-11-11 17:05:32 +0400

    Построение таблиц
    Теперь и скрипт генерящий таблицу работает правильно. Правда сейчас
    генерируется уже не одна таблица, а две - генерируется еще и таблица с
    данными для разбора modRM и immediate байтов. Но на данный момент эта
    информация не учитывается в самом дизасме. С другой стороны сейчас это
    вполне рабочая версия.
    Да, насчет коментов на русском - для этого нужна кодировка UTF8, а у
    меня было в ANSI. Проблема в том, что масм не понимает сорцы,
    закодированные в UTF8. Ну либо я нен ашел такой функции. Поэтому в
    ассемблерном коде все по прежнему печально с коментами.

--- build_table.pl
+++ build_table.pl
@@ -6,40 +6,64 @@ use diagnostics;
 
 #open my $in, "<", $fileName or die "$!";
 open my $in, "<", "instruction_list.txt" or die "$!";
-open my $output, ">", "state_table.txt" or die "wtf";
-open my $output, ">", "state_table.txt" or die "wtf";
+open my $outputState, ">", "state_table.txt" or die "wtf";
+open my $outputTail, ">", "modRM_and_immediate_table.txt" or die "wtf";
 
 my $state = 0;
 my $maxstate = 0;
-push my @table, [&getClearState];
+push my @stateTable, [&getClearState];
+my @tailTable;
 
 while (<$in>) {
 	chomp;
+	/modRM=(\d+)/;
+	my $modRM = $1;
+	my @imm = ();
+	push @imm, $1, $2 if(m'immediate=(\d+)\/(\d+)');
+	s@modRM.*@@;
 	my @bytes = split " ";
 	foreach my $b (@bytes) {
 		$b = hex $b;
-		if(0 != $table[$state][$b]){
-			$state = $table[$state][$b];
+		if(0 != $stateTable[$state][$b]){
+			$state = $stateTable[$state][$b];
 		} else {
 			$maxstate++;
-			push @table, [&getClearState];
-			$table[$state][$b] = $maxstate;
+			push @stateTable, [&getClearState];
+			$stateTable[$state][$b] = $maxstate;
 			$state = $maxstate;
 		}
 	}
+	$tailTable[$state] = [&getClearTail];
+	$tailTable[$state][0] = $modRM;
+	$tailTable[$state][1] = $imm[0];
+	$tailTable[$state][2] = $imm[1];
 	$state = 0;
 }
 
-foreach(@table) {
-	print $output "@$_";
-	print $output "\n";
+foreach(@stateTable) {
+	print $outputState "@$_";
+	print $outputState "\n";
+}
+foreach(@tailTable) {
+	if($_) {
+		print $outputTail "@$_";
+		print $outputTail "\n";
+	}
 }
 
 close $in;
-close $output;
+close $outputState;
+close $outputTail;
 sub getClearState {
 	my @res;
 	for(0x00..0xff) {
+	push @res, 0;
+	}
+	@res
+}
+sub getClearTail {
+	my @res;
+	for(0..2) {
 	push @res, 0;
 	}
 	@res
